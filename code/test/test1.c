936dcaeeb7609574cfa2af56ff96e9d775925b7e
zxd41082 <zxd41082@gmail.com>
2020-12-30 16:10:16 +0800

    test1_Revised

--- code/test/test1.c
+++ code/test/test1.c
@@ -1,7 +1,18 @@
 #include "syscall.h"
 main()
 	{
-		int	n;
+		/*int	n;
 		for (n=9;n>5;n--)
-			PrintInt(n);
+			PrintInt(n);*/
+		
+		int n;
+		int arr[25];
+		int sum = 0;
+		for (n=0;n<25;n++){
+			arr[n] = n;
+			sum += arr[n];
+			PrintInt(sum);
+		}
+		PrintInt(sum);
+		
 	}
